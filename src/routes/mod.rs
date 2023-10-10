20c8deefca4674ff3ca1fbcff0ddd0ca5b68f3b9
Robert Sheffield <sheffira2@gmail.com>
2023-10-09 19:07:27 -0700

    Messing with allowed origins to fix cors issues with web client. Clarified error message for missing auth token

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -18,7 +18,7 @@ mod users;
 
 pub async fn start_web(web_port: u16, db: Arc<PgPool>) -> Result<(), Error> {
     let cors = warp::cors()
-        .allow_origins(["http://localhost:5173"])
+        .allow_origins(["http://localhost:5173, localhost:5173"])
         .allow_headers(vec!["Supabase-Auth-Token", "Content-Type", "content-type"])
         .allow_methods(vec!["GET", "POST", "HEAD", "DELETE", "PATCH", "OPTIONS"]);
 
@@ -64,7 +64,7 @@ pub enum Error {
     #[error("Web server failed to start because web-folder '{0}' not found.")]
     FailStartWebFolderNotFound(String),
 
-    #[error{"Fail authentication missing X-Auth-Token header."}]
+    #[error{"Fail authentication missing Supabase-Auth-Token header."}]
     FailAuthMissingXAuth,
 }
 
